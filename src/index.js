c12000a9dff55388fe62b2f1b3a79fbc32cf2688
insuby <otto.voice@gmail.com>
2024-03-04 19:30:51 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { App } from 'app';
+
+import { createRoot } from 'react-dom/client';
+
+const element = document.getElementById('root');
+const root = createRoot(element);
+root.render(<App />);
